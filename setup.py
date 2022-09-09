6939a985b3c5fa8da16963a1c02e6d254f784392
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2022-09-09 14:53:52 -0700

    Create project template and repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Text mining drug labels',
+    author='USD ADS 509',
+    license='',
+)
