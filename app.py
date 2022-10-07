1dd2f40a13f9c7078539c4f64a5cc0e08726d308
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2022-10-07 10:40:58 -0700

    Create app and add tabs

---
+++ app.py
@@ -0,0 +1,30 @@
+from dash import Dash, dcc, html, Input, Output
+
+app = Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Text Mining Drug Labels'),
+    dcc.Tabs(id='selected-tab', value='classifier-tab', children=[
+        dcc.Tab(label='Classifier', value='classifier-tab'),
+        dcc.Tab(label='Topic Model', value='topic-model-tab')
+    ]),
+    html.Div(id='tab-content')
+])
+
+@app.callback(
+    Output('tab-content', 'children'),
+    Input('selected-tab', 'value')
+)
+def render_content(selected_tab):
+    if selected_tab == 'classifier-tab':
+        return html.Div([
+            html.H3('Classifier')
+        ])
+    elif selected_tab == 'topic-model-tab':
+        return html.Div([
+            html.H3('Topic Model')
+        ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
