3500616bc856c5e8a868b03a0d11d518edbda44e
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2022-10-11 14:35:57 -0700

    Don't render tab upon click

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import plotly.express as px
 from notebooks.cleaning import prepare, get_document_features
 
-### GLOBAL VARIABLES ###
-
 app = Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
 
@@ -16,51 +14,40 @@ with open('models/classifier.pkl', 'rb') as f:
 with open('models/classifier_features.pkl', 'rb') as f:
     classifier_features = pickle.load(f)
 
-### APP LAYOUT ###
-
 app.layout = html.Div([
     html.H1('Text Mining Drug Labels'),
-    dcc.Tabs(id='selected-tab', value='classifier-tab', children=[
-        dcc.Tab(label='Classifier', value='classifier-tab'),
-        dcc.Tab(label='Topic Model', value='topic-model-tab')
-    ]),
-    html.Div(id='tab-content')
-])
-
-### APP CALLBACKS ###
-
-@app.callback(
-    Output('tab-content', 'children'),
-    Input('selected-tab', 'value')
-)
-def render_content(selected_tab):
-    if selected_tab == 'classifier-tab':
-        return html.Div(style={'display': 'flex'}, children=[
-            html.Div(style={'width': '49%', 'display': 'inline-block'}, children=[
-                html.H3('Dosage and Administration Text'),
-                dcc.Textarea(
-                    id='input-text',
-                    style={'width': '99%', 'height': 370, 'resize': 'none'},
-                    value='DIRECTIONS Chew tablets and let dissolve in mouth. Do not use more than directed. Do not take with food.'
-                ),
-                html.Button('Classify', id='classify-button', n_clicks=0)
-            ]),
-            html.Div(style={'width': '49%', 'display': 'inline-block'}, children=[
-                html.H3('Predicted Route of Administration'),
-                html.Div(id='pdist')
+    dcc.Tabs([
+        dcc.Tab(label='Classifier', children=[
+            html.Div(style={'display': 'flex'}, children=[
+                html.Div(style={'width': '49%', 'display': 'inline-block'}, children=[
+                    html.H3('Dosage and Administration Text'),
+                    dcc.Textarea(
+                        id='input-text',
+                        style={'width': '99%', 'height': 370, 'resize': 'none'},
+                        value='DIRECTIONS Chew tablets and let dissolve in mouth. Do not use more than directed. Do not take with food.'
+                    ),
+                    html.Button('Classify', id='classify-button', n_clicks=0)
+                ]),
+                html.Div(style={'width': '49%', 'display': 'inline-block'}, children=[
+                    html.H3('Predicted Route of Administration'),
+                    html.Div(id='pdist')
+                ])
+            ])
+        ]),
+        dcc.Tab(label='Topic Model', children=[
+            html.Div([
+                html.Iframe(
+                    src=app.get_asset_url('lda_display.html'),
+                    style={
+                        'position': 'absolute',
+                        'width': '100%',
+                        'height': '100%'
+                    }
+                )
             ])
         ])
-    elif selected_tab == 'topic-model-tab':
-        return html.Div([
-            html.Iframe(
-                src=app.get_asset_url('lda_display.html'),
-                style={
-                    'position': 'absolute',
-                    'width': '100%',
-                    'height': '100%'
-                }
-            )
-        ])
+    ])
+])
 
 @app.callback(
     Output('pdist', 'children'),
@@ -79,8 +66,6 @@ def classify_text(n_clicks, text):
         }).sort_values(by='probability')
         fig = px.bar(pdist_df, y='route', x='probability')
         return [dcc.Graph(figure=fig)]
-
-### RUN APP ###
 
 if __name__ == '__main__':
     app.run_server(debug=True)
